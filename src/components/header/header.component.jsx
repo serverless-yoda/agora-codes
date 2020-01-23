d4dc4dbfb35cca2016fc3d0d8b95cacbfd6d56a4
manny i. agbayani <serverless.yoda@gmail.com>
2020-01-23 21:16:11 +1300

    added cart-icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import './header.style.scss'
 import { auth } from '../firebase/firebase.utils'
 
 import { connect } from 'react-redux'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 const Header = ({currentUser}) => (
     <div className="header">
@@ -27,6 +28,7 @@ const Header = ({currentUser}) => (
                 SIGN IN
                 </Link>
             }
+            <CartIcon/>
         </div>
     </div>
 )
