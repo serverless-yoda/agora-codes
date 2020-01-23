5b2a2f98702e22cf299edb8d69b623a719c7db6b
manny i. agbayani <serverless.yoda@gmail.com>
2020-01-23 20:52:13 +1300

    toggling sigin and signup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shops/shops.component'
@@ -48,7 +48,9 @@ class  App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndUp} />
+          <Route exact path="/signin" 
+            render= {() => this.props.currentUser ? (<Redirect to="/" />): (<SignInAndUp/>)}
+          />
         </Switch>
         
     </div>
@@ -57,7 +59,10 @@ class  App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
 const  mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
