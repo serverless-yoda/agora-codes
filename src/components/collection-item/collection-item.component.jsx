967ff156a7ad5712c822f257755bde6b1f2d8251
manny i. agbayani <serverless.yoda@gmail.com>
2020-01-24 21:39:23 +1300

    added cartItem in cartReducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,24 +2,33 @@ import React from 'react'
 
 import './collection-item.style.scss'
 import FormButton from '../form-button/form-button.component'
+import { connect } from 'react-redux'
+import { addItem } from '../../redux/cart/cart.action'
 
-const CollectionItem = ({id,name,price,imageUrl}) => (
-    <div className='collection-item'>
-        <div
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-            
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
+const CollectionItem = ({item,addItem}) => {
+    const {name,price,imageUrl} = item
 
-        <FormButton inverted>Add to cart</FormButton>
-        
-    </div>
-)
+    return (
+        <div className='collection-item'>
+                <div
+                    className='image'
+                    style={{
+                        backgroundImage: `url(${imageUrl})`
+                    }}
+                />
+                    
+                <div className='collection-footer'>
+                    <span className='name'>{name}</span>
+                    <span className='price'>{price}</span>
+                </div>
 
-export default CollectionItem
+                <FormButton inverted onClick={() => addItem(item)}>Add to cart</FormButton>
+                
+            </div>
+    )
+}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+export default connect(null,mapDispatchToProps)(CollectionItem)
