7a1c19072cc382c05f9f450aeccea8f4c21c707b
manny i. agbayani <serverless.yoda@gmail.com>
2020-01-26 19:02:03 +1300

    add data mapping

--- src/components/collection/collection.component.jsx
+++ src/components/collection/collection.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { selectShopSingleCollection } from '../../redux/shop/shop.selector'
+import { connect } from 'react-redux'
 
 import './collection.style.scss'
-
-const Collection =({match}) => {
-    console.log(match)
+import CollectionItem from '../collection-item/collection-item.component'
+const Collection =({collection}) => {
+    const {title, items} = collection
     return (
         <div className="collection-page">
-            <h1>Collection: {match.params.collectionId}</h1>
+            <h2 className="title">{title}</h2>
+            <div className="items">
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item} />)
+                }
+            </div>
         </div>
     )
 }
 
-export default Collection
+const mapStateToProps = (state,ownProps) => ({
+    collection: selectShopSingleCollection(ownProps.match.params.collectionId)(state)
+})
+export default connect(mapStateToProps)(Collection)
